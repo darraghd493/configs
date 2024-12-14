2eec1f7bc38b061a07364eca102bd0f1c3edf599
darraghd493 <60071719+darraghd493@users.noreply.github.com>
2024-12-14 16:34:30 +0000

    chore: exclude dist from tests
    Todo: Add dist back.
    Reason: Issue with mocking, can be fixed later.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
+  roots: ["<rootDir>/src/", "<rootDir>/test/"],
+  testPathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules"],
   testEnvironment: "node",
   transform: {
     "^.+.tsx?$": ["ts-jest",{}],
