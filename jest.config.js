e084144b8c376da674d38f218c6997e2ac9309d0
darraghd493 <60071719+darraghd493@users.noreply.github.com>
2024-12-14 17:04:56 +0000

    chore: convert jest.config.json
    This is my first Typescript package, I'm not surprised there is issues. XD

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-module.exports = {
-  roots: ["<rootDir>/src/", "<rootDir>/test/"],
-  testPathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules"],
-  testEnvironment: "node",
-  transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
-  },
-};
